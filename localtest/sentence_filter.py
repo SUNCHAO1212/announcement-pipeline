b9204aa0345cadc31da8289e0431c9963afe35fa
qinjia.zhu <qjzhu@quant-chi.com>
2018-05-21 10:08:47 +0800

    gitlib

--- localtest/sentence_filter.py
+++ localtest/sentence_filter.py
@@ -39,7 +39,7 @@ tables = re.compile('<lz.+?</lz>')
 
 def sent_filter(html):
     # sents = content.split('。')
-    # 丑陋的分段方式，用
+    # 丑陋的分段方式
     temp = re.sub(tables, '', html)
     sents = []
     temp = temp.split('一、')[-1]
@@ -59,38 +59,39 @@ def sent_filter(html):
     return res
 
 
-def html_section(html, class_='Section'):
-    """ BeautifulSoup 方法 """
-    bs = BeautifulSoup(html, 'lxml')
-    sections = bs.find_all(class_=class_)
-    # second_sections = bs.find_all(class_='Second-Section')
-
-    split_tag = []
-    for i, section in enumerate(sections):
-        print(section)
-        split_tag.append(section)
-    sents = []
-    for i, tag in enumerate(split_tag):
-        if i == 0:
-            former, html = html.split(tag)
-        else:
-            former, html = html.split(tag)
-            sents.append(former)
-    else:
-        sents.append(html)
-    if class_ == 'Section':
-        res = []
-        for sent in sents:
-            res.append(html_section(sent, class_='Second-Section'))
-        return res
-    elif class_ == 'Second-Section':
-        if sents:
-            return sents
-        else:
-            return [html]
+# def html_section(html, class_='Section'):
+#     """ BeautifulSoup 方法 """
+#     bs = BeautifulSoup(html, 'lxml')
+#     sections = bs.find_all(class_=class_)
+#     # second_sections = bs.find_all(class_='Second-Section')
+#
+#     split_tag = []
+#     for i, section in enumerate(sections):
+#         print(section)
+#         split_tag.append(section)
+#     sents = []
+#     for i, tag in enumerate(split_tag):
+#         if i == 0:
+#             former, html = html.split(tag)
+#         else:
+#             former, html = html.split(tag)
+#             sents.append(former)
+#     else:
+#         sents.append(html)
+#     if class_ == 'Section':
+#         res = []
+#         for sent in sents:
+#             res.append(html_section(sent, class_='Second-Section'))
+#         return res
+#     elif class_ == 'Second-Section':
+#         if sents:
+#             return sents
+#         else:
+#             return [html]
 
 
 def html_section_v2(html, class_='"Section"'):
+    # TODO 兼容多级目录，自动识别结构
     pt = re.compile('(<div class={}>.*?</div>)'.format(class_))
     split_tag = pt.findall(html)
     sents = []
